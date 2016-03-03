b72295d3b5567c102daa6fc6e0f6734b28b2c31c
Eduardo Cortes <cortes.z.eduardo@gmail.com>
2016-03-03 12:18:50 -0500

    Traducciones de mensajes

--- dist/chosen.jquery.js
+++ dist/chosen.jquery.js
@@ -465,11 +465,11 @@
       return true;
     };
 
-    AbstractChosen.default_multiple_text = "Select Some Options";
+    AbstractChosen.default_multiple_text = "seleccione una o varias opciones";
 
-    AbstractChosen.default_single_text = "Select an Option";
+    AbstractChosen.default_single_text = "seleccione una opción";
 
-    AbstractChosen.default_no_result_text = "No results match";
+    AbstractChosen.default_no_result_text = "No se encontraron resultados con";
 
     return AbstractChosen;
 
